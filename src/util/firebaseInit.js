391de5eeb4fc2acd8831cc7dc82d527bf50da2fa
Joel N. Walkley <joelnwalkley@gmail.com>
2021-03-06 10:41:11 -0500

    import analytics package

--- src/util/firebaseInit.js
+++ src/util/firebaseInit.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/firestore';
+import 'firebase/analytics';
 import { firebaseConfig } from './firebaseConfig';
 import { createContext } from 'react';
 
